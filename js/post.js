d2e6f674acaca5f72645ab8b592cd29db673dfc8
John Kindem <461425614@qq.com>
2017-12-11 20:14:56 +0800

    bug fix

--- js/post.js
+++ js/post.js
@@ -8,4 +8,11 @@ $(document).ready(function () {
     $('p').addClass('flow-text');
     $('#post-container').show();
     $('#footer').show();
+
+    // donate modal 初始化
+    $('.modal').modal();
+    // 绑定捐赠按钮事件
+    $('.donate-modal-trigger').click(function () {
+        $('#donate-modal').modal('open')
+    });
 });
