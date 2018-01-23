d790fdce7095d7394250dcfde89f6103841368b8
John Kindem <461425614@qq.com>
2018-01-23 12:58:40 +0800

    little 更新

---
+++ js/admin/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // 登录事件
+    $('#login-btn').ready(function () {
+        $.ajax({
+            url: "",    //TODO
+            async: true,
+            data: "username=" + $('#username').val() + "&password=" + $('#password').val()
+            dataType: 'json',
+            success: function (data) {
+                //TODO
+            }
+        })
+    });
+});
